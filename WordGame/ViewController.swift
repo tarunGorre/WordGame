f578c955531a6463edc1fe0270e5d8933cfbb68a
Tarun Gorre <gtarun16@gmail.com>
2019-07-16 22:37:26 +0200

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by Tarun Gorre on 16.07.19.
+//  Copyright © 2019 Tarun Gorre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
